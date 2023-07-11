e9183051536e5aa79a1b58f937a06910eb8be8c4
yaswanth58 <88507413+yaswanth58@users.noreply.github.com>
2023-07-11 22:14:34 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCafE5yZSg3NXaoH3c1aUinIATodhEggmM",
+    authDomain: "coin-desk-8bb92.firebaseapp.com",
+    projectId: "coin-desk-8bb92",
+    storageBucket: "coin-desk-8bb92.appspot.com",
+    messagingSenderId: "732768357045",
+    appId: "1:732768357045:web:a50794452597b5fba7ccfc"
+  };
+
+  export default firebaseConfig;
